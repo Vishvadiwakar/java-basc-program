eab084eebb74cb386714471523bd34f6aecb6699
Diwakar <diwakarvishva@gmail.com>
2024-03-03 15:56:02 +0530

    add add file

---
+++ add.java
@@ -0,0 +1,11 @@
+//package Programs.java-basic-program;
+
+public class add {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=5;
+        b=10;
+        c=a+b;
+        System.err.println("The sum is " +c);
+    }
+}
